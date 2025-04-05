41d9a28f1f3181f3ebd9aa3f9f0179918b1b02ac
m-divya-29 <divya.gracie@gmail.com>
2025-04-05 18:02:28 +0530

    Google cloud Run fixes

--- search-suggestions-backend/src/main/java/com/md29/search/suggestions/controllers/SuggestionController.java
+++ search-suggestions-backend/src/main/java/com/md29/search/suggestions/controllers/SuggestionController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/suggestion")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class SuggestionController {
     @Autowired
     private SuggestionService suggestionService;
